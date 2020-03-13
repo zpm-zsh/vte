dded88f73bed58186b53a9c0079f66cc8232aca4
Grigorii Horos <horosgrisa@gmail.com>
2020-03-13 20:25:20 +0200

    Initial commit

---
+++ vte.plugin.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+_zpm__vte_urlencode() (
+  # This is important to make sure string manipulation is handled
+  # byte-by-byte.
+  LC_ALL=C
+  str="$1"
+  while [ -n "$str" ]; do
+    safe="${str%%[!a-zA-Z0-9/:_\.\-\!\'\(\)~]*}"
+    printf "%s" "$safe"
+    str="${str#"$safe"}"
+    if [ -n "$str" ]; then
+      printf "%%%02X" "'$str"
+      str="${str#?}"
+    fi
+  done
+)
+
+_zpm__vte_osc7 () {
+  printf "\033]7;file://%s%s\033\\" "${HOSTNAME:-}" "$(_zpm__vte_urlencode "${PWD}")"
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd __vte_osc7
+__vte_osc7
